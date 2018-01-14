5bcd5e170708ce30ee2c0cb7f23eeaa2a9ce1297
Grant Schulte <schulte.grant@gmail.com>
2018-01-14 10:39:38 -0500

    Add HomePage menu for development

--- client/src/home/home.js
+++ client/src/home/home.js
@@ -1,12 +1,20 @@
 // @flow
 
 import * as React from "react";
+import { Link } from "react-router-dom";
 import "./Home.scss";
 
 function HomePage(): React.Node {
   return (
-    <div id="HomePage">
-      <h1>Hello</h1>
+    <div id="HomePage" className="page--padding">
+      <ul id="HomePage__menu">
+        <li>
+          <Link to="/lcars">LCARS</Link>
+        </li>
+        <li>
+          <Link to="/cards">Cards</Link>
+        </li>
+      </ul>
     </div>
   );
 }
