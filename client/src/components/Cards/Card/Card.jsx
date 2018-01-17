364a1806906f7d380af4117c9c9b4f0cebca2104
Grant Schulte <schulte.grant@gmail.com>
2018-01-17 07:09:25 -0500

    Card outline

--- client/src/components/Cards/Card/Card.jsx
+++ client/src/components/Cards/Card/Card.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import * as React from "react";
+import "./Card.scss";
 
 type Props = {
 
@@ -8,8 +9,24 @@ type Props = {
 
 function Card(props: Props): React.Node {
   return (
-    <div className="card">
-      SUP
+    <div className="Card">
+      <div className="Card__inner">
+        <div className="Card__heading">
+          <span className="Card__heading__affiliation"></span>
+          <span className="Card__heading__title"></span>
+        </div>
+
+        <div className="Card__image">
+
+        </div>
+
+        <div className="Card__summary">
+          <div className="Card__summary__type"></div>
+        </div>
+
+        <div className="Card__skills"></div>
+        <div className="Card__attributes"></div>
+      </div>
     </div>
   );
 }
